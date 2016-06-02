d24a91af37710eafea13b95868878d767b52eac1
perlancar (@pc-office) <perlancar@gmail.com>
2016-06-02 16:05:00 +0700

    Initial commit

---
+++ lib/App/ZodiacUtils/Lite.pm
@@ -0,0 +1,25 @@
+package App::ZodiacUtils::Lite;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: CLI utilities related to zodiac (Perinci::CmdLine::Lite version)
+
+=head1 DESCRIPTION
+
+This distribution is only for testing (originally, to test
+L<Dist::Zilla::Plugin::Rinci::AddPrereqs>).
+
+This distribution includes the following CLI utilities:
+
+#INSERT_EXECS_LIST
+
+They are L<Perinci::CmdLine::Lite>-based versions of the same CLI utilities
+distributed in L<App::ZodiacUtils> (which are
+L<Perinci::CmdLine::Inline>-based).
+
+
+=head1 SEE ALSO
+
+L<App::ZodiacUtils>
